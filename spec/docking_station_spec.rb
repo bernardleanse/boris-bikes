60fda024fb5be7e4073a93a034f25ad116a0910e
Alex Boyling <alexboyling@gmail.com>
2021-09-21 15:50:29 +0100

    Changed to one-liner syntax

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,8 +1,5 @@
 require 'docking_station'
 
 describe DockingStation do
-  docking_station = DockingStation.new
-  it "Respond to release_bike" do
-    docking_station.release_bike
-  end
+  it { is_expected.to respond_to :release_bike }
 end
