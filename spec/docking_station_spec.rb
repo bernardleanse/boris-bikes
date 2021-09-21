2ef7b8c2c6f5790f5f70650419c1b3047387d21b
Alex Boyling <alexboyling@gmail.com>
2021-09-21 16:48:28 +0100

    Added feature test for bike release

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,5 +1,12 @@
 require 'docking_station'
 
 describe DockingStation do
+  dock = DockingStation.new
   it { is_expected.to respond_to :release_bike }
+  it "Should return a Bike object when releasing" do
+    expect(dock.release_bike).to be_instance_of(Bike)
+  end
+  it "The returned bike should be working" do
+    expect(dock.release_bike.working?).to eq true
+  end
 end
