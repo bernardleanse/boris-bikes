a48c164f5611eef2adf4c1a2bfea65ae0a4a0fb0
Bernard Leanse <bernardleanse96@gmail.com>
2021-09-22 15:14:09 +0100

    Added ability to pass in a bike to the station and see docked bikes

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,5 +1,14 @@
 class DockingStation
+  attr_reader :docked_bikes
+  def initialize
+    @docked_bikes = Array.new
+  end
+
   def release_bike
     return Bike.new
+  end
+
+  def dock_bike(bike)
+    @docked_bikes << bike
   end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -6,7 +6,22 @@ describe DockingStation do
   it "Should return a Bike object when releasing" do
     expect(dock.release_bike).to be_instance_of(Bike)
   end
+
   it "The returned bike should be working" do
     expect(dock.release_bike.working?).to eq true
   end
+
+  it { is_expected.to respond_to(:dock_bike).with(1).argument }
+
+  it "should allow docking of a bike" do
+    bike = Bike.new
+    expect(dock.dock_bike(bike).first).to eq(bike)
+  end
+
+  it "should show docked bikes" do
+    expect(dock.docked_bikes).to be_instance_of Array
+    expect(dock.docked_bikes.first).to be_instance_of Bike
+  end
+  
+  
 end
